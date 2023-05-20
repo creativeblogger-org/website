e176a3d1cf0fbbbf9a45780d08338d50a14e1633
RIC217 <melericcardo9400@gmail.com>
2023-05-20 17:14:46 +0200

    Corrected tiny bug

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,6 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 import App from './App';
+import { Router } from '@solidjs/router';
 
-const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
-  );
-}
-
-render(() => <App />, root!);
+render(() => <Router><App /></Router>, document.getElementById('root') as HTMLElement);
