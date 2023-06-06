59832ffa0ddc006f71f8aaae1e9c29b3a2cc9020
RIC217 <melericcardo9400@gmail.com>
2023-06-06 10:00:38 +0200

    Patched tiny display bug

--- src/components/CreatePostComponent.tsx
+++ src/components/CreatePostComponent.tsx
@@ -4,7 +4,7 @@ import { NavLink } from "@solidjs/router";
 const CreatePostComponent: Component = () => {
   return (
     <NavLink href="/create"
-      class="bg-teal-500 duration-150 hover:bg-indigo-500 rounded-full border p-4 text-5xl fixed right-0 bottom-0"
+      class="bg-teal-500 duration-150 hover:bg-indigo-500 rounded-full border p-4 text-black text-5xl fixed right-0 bottom-0"
     >
       +
     </NavLink>
