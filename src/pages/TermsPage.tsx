df1b975a771f6bd38241576a999dec6695526d39
RIC217 <melericcardo9400@gmail.com>
2023-08-17 18:16:39 +0200

    Updated privacy policy

--- src/pages/TermsPage.tsx
+++ src/pages/TermsPage.tsx
@@ -12,10 +12,10 @@ const TermsPage: Component = () => {
       <div class="flex justify-center">
         <h1>Politique de confidentialité & conditions d'utilisation</h1>
         <p class="p-2 rounded-md border m-5 w-1/2">
-          Veuillez rester cordial dans vos propos. Vous pouvez y exprimer vos
-          opinions tout en respectant celles des autres.
+          Veuillez rester cordial dans vos propos. Vous pouvez y exprimer vos opinions tout en respectant celles des autres.
           <br />
-          Vous seul êtes responsable de ce que vous postez, tout contenu inapproprié sera supprimé et l'auteur pourra être sanctionné par un bannissement temporaire ou définitif. Vous pouvez modifier ou supprimer vos posts et commentaires qui, dans ce cas, seront supprimés de la base de données.
+          Vous êtes le seul responsable du contenu que vous postez, tout contenu inapproprié sera supprimé et l'auteur pourra être sanctionné par un bannissement temporaire ou définitif. <br />
+          Vous pouvez modifier ou supprimer vos posts et commentaires qui, dans ce cas, seront supprimés définitivement de la base de données.
           <br />
           Creative Blogger Org ne peut en aucun cas être tenu responsable du contenu posté par ses membres.
           <br />
@@ -25,7 +25,7 @@ const TermsPage: Component = () => {
           1. Votre nom d'utilisateur<br />
           2. Votre adresse e-mail<br />
           3. Votre mot de passe (dont seul le hash est stocké pour des raisons de sécurité)<br />
-          4. Votre date de naissance
+          4. Votre date de naissance afin de protéger les enfants des contenus pouvant être inappropriés
         </p>
       </div>
     </MetaProvider>
