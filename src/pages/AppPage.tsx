3f5d82a65f6677d8b89206e9f18295d29c06cb60
mael <mael@pop-os.localdomain>
2023-10-25 18:22:47 +0200

    some little news

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -10,32 +10,32 @@ const AppPage: Component = () => {
         L'ensemble de Creative Blogger dans une app
       </h1>
       <div class="">
-        <div class="flex justify-center">
+        <div class="flex sm:justify-center mb-10">
           <img
             src={MobileApp1}
-            class="rounded-xl h-96 m-0 p-0 mx-10"
+            class="rounded-xl h-96 m-0 p-0 mx-3 sm:mx-10"
             alt="Image de l'app mobile affichant les articles"
           />
-          <div class="w-1/5 text-3xl">
+          <div class="w-1/3 sm:w-1/5 text-xl sm:text-3xl">
             <h2>
               Retrouvez tous les articles de Creative Blogger centralisés en un
               endroit dans votre téléphone !
             </h2>
             <br />
             <a
-              class="py-5 bg-teal-500 text-white p-2 text-2xl rounded-3xl font-bold duration-200 hover:bg-teal-600"
+              class="py-5 bg-teal-500 text-white p-1 sm:p-2 text-sm sm:text-2xl rounded-3xl sm:font-bold duration-200 hover:bg-teal-600"
               href="https://play.google.com/store/apps/details?id=org.creativeblogger.org&hl=fr"
             >
               Téléchargez-là
             </a>
           </div>
         </div>
-        <div class="flex justify-center my-20">
-          <div class="w-1/5 text-3xl">
+        <div class="hidden sm:flex justify-center my-20">
+          <div class="w-1/3 sm:w-1/5 text-xl sm:text-3xl">
             <h2>Éditez votre profile directement depuis l'app !</h2>
             <br />
             <a
-              class="py-5 bg-indigo-500 text-white p-2 text-2xl rounded-3xl font-bold duration-200 hover:bg-indigo-600"
+              class="py-5 bg-indigo-500 text-white p-1 sm:p-2 text-sm sm:text-2xl rounded-3xl font-bold duration-200 hover:bg-indigo-600"
               href="https://play.google.com/store/apps/details?id=org.creativeblogger.org&hl=fr"
             >
               Téléchargez-là
@@ -43,17 +43,21 @@ const AppPage: Component = () => {
           </div>
           <img
             src={MobileApp2}
-            class="rounded-xl h-96 m-0 p-0 mx-10"
+            class="rounded-xl h-96 m-0 p-0 mx-3 sm:mx-10"
             alt="Image de l'app mobile sur la page profile en train de modifier son profile"
           />
         </div>
-        <div class="flex justify-center mb-10">
-          <img src={MobileApp3} class="rounded-xl h-96 m-0 p-0 mx-10" alt="" />
-          <div class="w-1/5 text-3xl">
+        <div class="flex sm:justify-center mb-10">
+          <img
+            src={MobileApp3}
+            class="rounded-xl h-96 m-0 p-0 mx-3 sm:mx-10"
+            alt=""
+          />
+          <div class="w-1/3 sm:w-1/5 text-xl sm:text-3xl">
             <h2>Lisez confortablement vos articles favoris !</h2>
             <br />
             <a
-              class="py-5 bg-teal-500 text-white p-2 text-2xl rounded-3xl font-bold duration-200 hover:bg-teal-600"
+              class="py-5 bg-teal-500 text-white p-1 sm:p-2 text-sm sm:text-2xl rounded-3xl sm:font-bold duration-200 hover:bg-teal-600"
               href="https://play.google.com/store/apps/details?id=org.creativeblogger.org&hl=fr"
             >
               Téléchargez-là
